568ac280436044bcad96e9b3b168e4013f8d0cf1
Doyle Ravnaas <doylerav@ccs.neu.edu>
2015-10-17 14:29:45 -0700

    various data and solr updates
    Added year column (and added year to id) in the "old" sample csv
    Added a 1000 row sample csv (also with year column and year in id)
    Made a minor change to schema (turns out live service count is a float
    for at least one of the rows in the larger 1000 row sample)
    Made the config batch files allow collection and dir as input

--- Solr/zkcli-dwncfg.bat
+++ Solr/zkcli-dwncfg.bat
@@ -1,3 +1,9 @@
-set ZKCLI_HOME=%solr_home%\server\scripts\cloud-scripts
-if not exist curcfg\. mkdir curcfg
-call %solr_home%\server\scripts\cloud-scripts\zkcli -zkhost localhost:9983 -cmd downconfig -confdir curcfg -confname csvtest
+if .%ZKCLI_HOME%. == .. set ZKCLI_HOME=%solr_home%\server\scripts\cloud-scripts
+set collection=%1
+if .%1. == .. set collection=csvtest
+set cfgdir=%2
+if .%2. == .. set cfgdir=curcfg
+
+@echo Downloading config for collection %collection% to directory %cfgdir%
+if not exist %cfgdir%\. mkdir %cfgdir%
+call %solr_home%\server\scripts\cloud-scripts\zkcli -zkhost localhost:9983 -cmd downconfig -confdir %cfgdir% -confname %collection%
--- Solr/zkcli-upcfg.bat
+++ Solr/zkcli-upcfg.bat
@@ -1,2 +1,19 @@
-set ZKCLI_HOME=%solr_home%\server\scripts\cloud-scripts
-call %solr_home%\server\scripts\cloud-scripts\zkcli -zkhost localhost:9983 -cmd upconfig -confdir curcfg -confname csvtest
+@if .%ZKCLI_HOME%. == .. set ZKCLI_HOME=%solr_home%\server\scripts\cloud-scripts
+@set collection=%1
+@if .%1. == .. set collection=csvtest
+@set cfgdir=%2
+@if .%2. == .. set cfgdir=curcfg
+
+@echo.
+@echo Uploading config for collection %collection% from directory %cfgdir%
+@echo.
+
+@set fileerr=1
+@if exist %cfgdir%\schema.xml if exist %cfgdir%\solrconfig.xml set fileerr=
+
+@if .%fileerr%. == .. call %solr_home%\server\scripts\cloud-scripts\zkcli -zkhost localhost:9983 -cmd upconfig -confdir curcfg -confname csvtest
+@if .%fileerr%. == .1. @echo Could not find %cfgdir%\schema.xml or %cfgdir%\solrconfig.xml
+@echo.
+
+@rem to do - add a small wait and then hit this url to reset the index of the collection?
+@rem http://localhost:8983/solr/admin/collections?action=RELOAD&name=csvtest&reindex=true&deleteAll=true
