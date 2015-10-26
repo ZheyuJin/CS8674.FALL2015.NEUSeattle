8881d4001e6c6a3bb3146def23eeff8e34b22534
doylerav <doylerav@gmail.com>
2015-10-26 15:35:15 -0700

    restructured project

--- WebPage/simple-medicare-request/src/org/hunter/medicare/controller/AjaxController.java
+++ WebPage/simple-medicare-request/src/org/hunter/medicare/controller/AjaxController.java
@@ -18,36 +18,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/main/ajax")
 public class AjaxController {
 
-	protected static Logger logger = Logger.getLogger("controller");
-	
-	@Resource(name="springService")
-	private StringAppend springService;
-	
-	/**
-	 * Handles and retrieves the AJAX Add page
-	 */
+  protected static Logger logger = Logger.getLogger("controller");
+  
+  @Resource(name="springService")
+  private StringAppend springService;
+  
+  /**
+   * Handles and retrieves the AJAX Add page
+   */
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String getAjaxAddPage() {
-    	logger.debug("Received request to show AJAX, add page");
-    	
-    	// This will resolve to /WEB-INF/jsp/ajax-add-page.jsp
-    	return "ajax-add-page";
-	}
+      logger.debug("Received request to show AJAX, add page");
+      
+      // This will resolve to /WEB-INF/jsp/ajax-add-page.jsp
+      return "ajax-add-page";
+  }
  
     /**
      * Handles request for adding two numbers
      */
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public @ResponseBody String add(@RequestParam(value="gender", required=true) String str1,
-    							@RequestParam(value="state", required=true) String str2,
-    							Model model) {
-		logger.debug("Received request to add two numbers");
-		
-		// Delegate to service to do the actual adding
-		String output = springService.add(str1, str2);
-		
-		// @ResponseBody will automatically convert the returned value into JSON format
-		// You must have Jackson in your classpath
-		return output;
-	}
+                  @RequestParam(value="state", required=true) String str2,
+                  Model model) {
+    logger.debug("Received request to add two numbers");
+    
+    // Delegate to service to do the actual adding
+    String output = springService.add(str1, str2);
+    
+    // @ResponseBody will automatically convert the returned value into JSON format
+    // You must have Jackson in your classpath
+    return output;
+  }
 }
