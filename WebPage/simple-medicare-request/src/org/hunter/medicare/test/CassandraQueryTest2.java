c1def36c62ff4bda207534d274f2a0f432dabc5d
timothycowley <timothycowley@gmail.com>
2015-10-29 01:36:41 -0700

    testing all fields

--- WebPage/simple-medicare-request/src/org/hunter/medicare/test/CassandraQueryTest2.java
+++ WebPage/simple-medicare-request/src/org/hunter/medicare/test/CassandraQueryTest2.java
@@ -32,7 +32,32 @@ public class CassandraQueryTest2 {
 //		}
 	CassandraQueryResponse cqr = new CassandraQueryResponse();
 	Provider test = cqr.getProviderById("1003000522F992132012");
-	System.out.println("provider npi is: " + test.npi);
+	System.out.println("id is: " + test.id);
+	System.out.println("year: " + test.year);
+	System.out.println("day service count: " + test.beneficiaries_day_service_count);
+	System.out.println("bene unique cnt: " + test.beneficiaries_unique_count);
+	System.out.println("lastname: " + test.last_or_org_name);
+	System.out.println("firstname: " + test.first_name);
+	System.out.println("zip: " + test.zip);
+	System.out.println("city: " + test.city);
+	System.out.println("state: " + test.state);
+	System.out.println("country " + test.country);
+	System.out.println("credentials " + test.credentials);
+	System.out.println("entity code" + test.entity_code);
+	System.out.println("npi is: " + test.npi);
+	System.out.println("provider type" + test.provider_type);
+	System.out.println("place of service" + test.place_of_service);
+	System.out.println("hcpcs code: " + test.hcpcs_code);
+	System.out.println("hcpcs description: " + test.hcpcs_description);
+	System.out.println("gender: " + test.providerDetails.gender);
+	System.out.println("street1: " + test.providerDetails.streetAddress1);
+	System.out.println("street2: " + test.providerDetails.streetAddress2);
+	System.out.println("participation: " + test.providerDetails.medicare_participation);
+	System.out.println("line count: " + test.line_service_count);
+	System.out.println("avg medicare payment :" + test.providerDetails.averageMedicarePaymentAmount);
+	System.out.println("avg medicare allowed :" + test.providerDetails.averageMedicareAllowedAmount);
+	System.out.println("hcpcs_drug_indicator is: " + test.providerDetails.hcpcs_drug_indicator);
+	System.out.println("avg submitted charge is " + test.providerDetails.averageSubmittedChargeAmount);
 	
 	}
  
