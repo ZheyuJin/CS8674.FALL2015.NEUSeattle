9f6dc61556197e7a2a5cfbf87e791057af22cde2
ahjorgen167 <ahjorgen167@gmail.com>
2015-11-14 14:05:00 -0800

    Removed outdated libraries from ML controller

--- WebPage/simple-medicare-request/src/org/hunter/medicare/controller/MachineLearning1Controller.java
+++ WebPage/simple-medicare-request/src/org/hunter/medicare/controller/MachineLearning1Controller.java
@@ -1,14 +1,6 @@
 package org.hunter.medicare.controller;
 
-import java.io.IOException;
-
-import java.util.*;
 import org.apache.log4j.Logger;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.hunter.medicare.data.CassandraQueryResponse;
-import org.hunter.medicare.data.Procedure;
-import org.hunter.medicare.data.Provider;
-import org.hunter.medicare.data.SolrProviderSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/ml1")
 public class MachineLearning1Controller {
-	static Logger logger = Logger.getLogger("PagingController");
+	static Logger logger = Logger.getLogger("MachineLearning1Controller");
 
 	@RequestMapping(value = "/request", method = RequestMethod.GET)
 	public String getCase1Form() {
