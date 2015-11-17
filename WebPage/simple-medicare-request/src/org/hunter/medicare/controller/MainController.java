84e2a992c685ffe38632598d6877cd4ad2646aff
Doyle Ravnaas <doylerav@ccs.neu.edu>
2015-11-17 06:41:47 -0800

    Added a simple generic error page

--- WebPage/simple-medicare-request/src/org/hunter/medicare/controller/MainController.java
+++ WebPage/simple-medicare-request/src/org/hunter/medicare/controller/MainController.java
@@ -19,6 +19,7 @@ import org.hunter.medicare.data.Provider;
 import org.hunter.medicare.data.SolrProviderSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -457,6 +458,22 @@ public class MainController {
             throw e;
         }
 
+    }
+
+    // Test the exception page
+    @RequestMapping(value = "/exception", method = RequestMethod.GET)
+    public void getExceptionPage() throws Exception {
+        throw new Exception("This is an error");
+    }
+
+    @ExceptionHandler({ Exception.class })
+    public String genericError() {
+        // Returns the logical view name of an error page, passed to
+        // the view-resolver(s) in usual way.
+        // See
+        // https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc
+        // for more options.
+        return "genericError";
     }
 }
 
