e494d708c74e17858004a8e9321fe3c1c137d7b5
timothycowley <timothycowley@gmail.com>
2015-11-18 18:32:39 -0800

    setup to connect to Tim EC2

---
+++ WebPage/simple-medicare-request/src/org/hunter/medicare/test/CassandraQueryTest3.java
@@ -0,0 +1,23 @@
+package org.hunter.medicare.test;
+
+import java.util.List;
+
+import org.hunter.medicare.data.CassandraQueryResponse;
+import org.hunter.medicare.data.ProcedureDetails;
+
+public class CassandraQueryTest3 {
+
+    public static void main(String[] args) {
+	CassandraQueryResponse cr = new CassandraQueryResponse();
+	List<ProcedureDetails> pr = cr.getPatientResponsibility(true, 10);
+	List<ProcedureDetails> pg = cr.getChargedMedicarePayGap(true, 10);
+	for (ProcedureDetails i : pr) {
+	    System.out.println(i.medicarePay);
+	}
+	for (ProcedureDetails i : pg) {
+	    System.out.println(i.medicarePay);
+	}
+
+    }
+
+}
