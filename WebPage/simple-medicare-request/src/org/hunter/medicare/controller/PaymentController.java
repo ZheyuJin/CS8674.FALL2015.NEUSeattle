15bd2fe5256dd50426d132c78dfb4aa2f5d58e7d
doylerav <doylerav@gmail.com>
2015-12-02 17:19:47 -0800

    better names for patient payment controller and methods

---
+++ WebPage/simple-medicare-request/src/org/hunter/medicare/controller/PaymentController.java
@@ -0,0 +1,59 @@
+package org.hunter.medicare.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hunter.medicare.data.ProcedureDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/payment")
+public class PaymentController {
+    static Logger logger = Logger.getLogger("PaymentController");
+
+    @RequestMapping(value = "/patient", method = RequestMethod.GET)
+    public String getQueryForm() {
+        System.out.println("Initialized Cassandra query page");
+        return "cassandra23-form";
+    }
+
+    @RequestMapping(value = "/request", method = RequestMethod.GET, params = { "numRows", "start",
+            "sortDesc", "isPercentage" })
+    public @ResponseBody List<ProcedureDetails> getPatientPaymentResponsibility(
+            @RequestParam(value = "numRows", required = true, defaultValue = "10") int numRows,
+            @RequestParam(value = "start", required = false, defaultValue = "true") int start,
+            @RequestParam(value = "sortDesc", required = false, defaultValue = "true") boolean sortDesc,
+            @RequestParam(value = "isPercentage", required = false, defaultValue = "false") boolean isPercentage)
+                    throws Exception {
+        System.out.println("Received Cassandra2/3 Request");
+        System.out.println("Reading numRows as " + Integer.toString(numRows) + " start as "
+                + Integer.toString(start) + " sortDesc as " + Boolean.toString(sortDesc)
+                + " and isPercentage " + Boolean.toString(isPercentage));
+        if (isPercentage) {
+        } else {
+        }
+        MainController main = new MainController();
+        List<ProcedureDetails> procedures = new ArrayList<ProcedureDetails>();
+        try {
+            int end = start + numRows - 1;
+
+            if (isPercentage) {
+                procedures = main.getProcedurePatientCopay(sortDesc, start, end);
+            } else {
+                procedures = main.getProcedureMedicarePayGap(sortDesc, start, end);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return procedures;
+    }
+
+}
