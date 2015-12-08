023cfd67a020034fe323ec9d202c9f826bf9fb30
ahjorgen167 <ahjorgen167@gmail.com>
2015-12-07 17:49:32 -0800

    Removed all references to case/assessment in Basic Procedure page

--- WebPage/simple-medicare-request/WebContent/js/original.js
+++ WebPage/simple-medicare-request/WebContent/js/original.js
@@ -13,8 +13,6 @@ $(document).ready(function() {
 	$('.table').hide();
 });
 
-
-
 $(document).on('click', '#submitSearch', function() {
 	$('.table').hide();
 	$('.table').DataTable().destroy();
@@ -104,25 +102,6 @@ function fillExpensiveTable(data){
 	} );
 }
 
-	/*
-	 * 	********* CASE 1 - EXPENSIVE *********** 
-				+ toNameCase(data[i].last_or_org_name)
-				+ toNameCase(data[i].first_name)
-				+ data[i].providerDetails.averageSubmittedChargeAmount
-						.toFixed(2) + '</td></tr>';
-
-	********* CASE 2 - BUSIEST ***********
-	for (var i = 0; i < data.length; i++) {
-		text += '<tr><td>' + toNameCase(data[i].last_or_org_name) + '</td><td>'
-				+ toNameCase(data[i].first_name) + '</td><td>'
-				+ data[i].beneficiaries_day_service_count + '</td></tr>';
-
-		****** CASE 3 - AVERAGE COST ******
-		text += '<tr><td>' + data[i].procCode + '</td><td>' + data[i].desc
-				+ '</td><td>\$' + data[i].avgCost.toFixed(2) + '</td><td>'
-				+ data[i].state + '</td></tr>';
-
-*/
 function checkEmpty(data) {
 	if (data.length == 0) {
 		return true;
@@ -131,15 +110,12 @@ function checkEmpty(data) {
 	}
 }
 
-$(document).on('click', '.case-select', function(){
-//function caseCheck() {
+$(document).on('click', '.request-select', function(){
 	if ($(this).is('#avgProcedureCost')) {
 		$('.keyword_class').show();
 		$('#proc_code').attr('disabled',true);
-		//document.getElementById('proc_code').disabled = true;
 	} else {
 		$('.keyword_class').hide();
 		$('#proc_code').attr('disabled', false);
-		//document.getElementById('proc_code').disabled = false;
 	}
 });
